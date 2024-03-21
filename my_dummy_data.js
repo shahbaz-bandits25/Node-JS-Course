bfbf16088799950e3f52be51e133f52ade940f52
shahbaz-bandits25 <shahbaz_djrian@hotmail.com>
2024-03-21 16:04:53 +0500

    Basic API created with file data and static data

---
+++ my_dummy_data.js
@@ -0,0 +1,5 @@
+
+const data = [{city:'Karachi',State:'Sindh',Country:'Pakistan'},
+              {city:'Paris',State:'Paris',Country:'France'},
+              {city:'Lahore',State:'Punjab',Country:'Pakistan'},]
+module.exports = data;
