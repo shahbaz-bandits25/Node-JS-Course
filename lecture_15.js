7bc5831b89cd7d0878d6c5c6c4d4cf5a86fe301f
shahbaz-bandits25 <shahbaz_djrian@hotmail.com>
2024-03-28 16:42:57 +0500

    Promisses

--- lecture_15.js
+++ lecture_15.js
@@ -11,11 +11,33 @@
 // will run first and then  Task-2 run which is the example of asynchronous program
 // means task-1 and task-3 not waited for the complete of task-2
 
-console.log(' Task-1')
-setTimeout(()=>{
-    console.log(' Task-2')
+// console.log(' Task-1')
+// setTimeout(()=>{
+//     console.log(' Task-2')
 
+// });
+// console.log(' Task-3')
+
+
+
+//Video number 15 here or lecture number 16 here
+//in above code we've a problem that Task-2 is delaying
+//To resolve these asynchronous issues we use promise 
+//below is the implementation 
+
+let a=10;
+let b=0;
+
+let waitnigData = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+
+        b=30;
+        resolve(b);
+    
+    },2000);
 })
-console.log(' Task-3')
 
-
+waitnigData.then((data)=>{
+    b= data;
+    console.log(a+b)
+})
